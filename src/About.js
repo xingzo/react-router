466edc0c2315349b12838bb0faa27bf739e36fb6
xingzo <kingsleynyaosi@gmail.com>
2018-03-27 09:55:15 -0700

    initial working react SPA

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render () {
+        return (
+            <div>
+                All about stocks. Read it here, folks!
+            </div>
+        )
+    }
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom'
+
+import About from './About';
+
+class App extends Component {
+  render() {
+    return (
+                <header>
+                    <h1>Learn Routing</h1>
+                    <nav>
+                        {/* Create our nav bar links using the Link element from react router */}
+                        <ul>
+                            <li><Link to="/about">About</Link></li>
+                        </ul>
+                    </nav>
+
+                      {/* Create the routes. This will not appear on the page. */}
+                    <div className="main">
+                        <Switch>
+                            <Route path="/about" component={ About } />
+                        </Switch>
+                    </div>
+                </header>
+    );
+  }
+}
+
+export default App;
